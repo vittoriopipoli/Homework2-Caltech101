7b869d9b3c9021424436a25b3ffc47f739492fdd
vittoriopipoli <64205156+vittoriopipoli@users.noreply.github.com>
2020-04-23 14:53:06 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -6,7 +6,7 @@ import os
 import os.path
 import sys
 
-#porcodio
+#porcodio cane
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
